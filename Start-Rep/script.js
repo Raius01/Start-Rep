7134167e1b0579f72a00ab29721d041ac432b674
Raius01 <ilya.odin1@yandex.ru>
2018-02-24 16:01:21 +0300

    sdweio

---
+++ Start-Rep/script.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("sandbox"),
+	context = canvas.getContext("2d"),
+	square, circle;
+square = new Path2D();
+square.moveTo(50, 50);
+square.lineTo(250, 50);
+square.lineTo(250, 250);
+square.lineTo(50, 250);
+square.lineTo(50, 50);
+
+context.lineWidth = 5;
+
+context.stroke(square);
+
+circle = new Path2D();
+circle.arc(150, 150, 100, 0, 2*Math.PI);
+
+context.fillStyle="red";
+context.fill(circle);
